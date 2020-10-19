7fbbd68cb92102c3f2a313df74e2534d8121f211
NathanLM <nathan.lemerrer@orange.fr>
2020-10-19 18:36:13 +0200

    Gradle setup & Golden master

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -11,7 +11,6 @@ internal class GildedRoseTest {
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("fixme", app.items[0].name)
-
     }
 
 }
--- src/test/kotlin/com/gildedrose/TexttestFixture.kt
+++ src/test/kotlin/com/gildedrose/TexttestFixture.kt
@@ -1,36 +1,55 @@
 package com.gildedrose
 
+
+private fun getNewItemList() = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+    Item("Aged Brie", 2, 0), //
+    Item("Elixir of the Mongoose", 5, 7), //
+    Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // this conjured item does not work properly yet
+    Item("Conjured Mana Cake", 3, 6))
+
 fun main(args: Array<String>) {
-
-    println("OMGHAI!")
-
-    val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
-            Item("Aged Brie", 2, 0), //
-            Item("Elixir of the Mongoose", 5, 7), //
-            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            // this conjured item does not work properly yet
-            Item("Conjured Mana Cake", 3, 6))
-
-    val app = GildedRose(items)
+    val items = getNewItemList()
+    val itemsLegacy = getNewItemList()
 
     var days = 2
-    if (args.size > 0) {
+    if (args.isNotEmpty()) {
         days = Integer.parseInt(args[0]) + 1
     }
 
-    for (i in 0..days - 1) {
-        println("-------- day $i --------")
-        println("name, sellIn, quality")
-        for (item in items) {
-            println(item)
-        }
-        println()
+    println("------------ Legacy ------------")
+    testLegacyVersion(itemsLegacy, days)
+
+    println("------------ Refactored ------------")
+    testRefactoredVersion(items, days)
+}
+
+
+private fun testRefactoredVersion(items: Array<Item>, days: Int){
+    val app = GildedRose(items)
+    for (i in 0 until days) {
+        printListContent(i, items)
         app.updateQuality()
     }
+}
 
+private fun testLegacyVersion(items: Array<Item>, days: Int){
+    val app = GildedRose(items)
+    for (i in 0 until days) {
+        printListContent(i, items)
+        app.updateQualityLegacy()
+    }
+}
 
+private fun printListContent(i: Int, items: Array<Item>){
+    println("-------- day $i --------")
+    println("name, sellIn, quality")
+    for (item in items) {
+        println(item)
+    }
+    println()
 }

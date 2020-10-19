cc2ed16e2823ec78610db1916190938ce9ec8907
NathanLM <nathan.lemerrer@orange.fr>
2020-10-19 18:51:24 +0200

    Began refactoring : break code into smaller functions

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,52 +5,54 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
+                decreaseQuality(items, i)
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                            increaseQuality(items, i)
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                            decreaseQuality(items, i)
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+            decreaseSellIn(items, i)
 
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
+                        decreaseQuality(items, i)
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        items[i].quality -= items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    decreaseQuality(items, i)
                 }
             }
+        }
+    }
+
+    private fun decreaseSellIn(items: Array<Item>, i: Int){
+        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            items[i].sellIn--
+        }
+    }
+
+    private fun increaseQuality(items: Array<Item>, i: Int){
+        if (items[i].quality < 50 && items[i].name != "Sulfuras, Hand of Ragnaros") {
+            items[i].quality++
+        }
+    }
+
+    private fun decreaseQuality(items: Array<Item>, i: Int){
+        if (items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros") {
+            items[i].quality--
         }
     }
 
